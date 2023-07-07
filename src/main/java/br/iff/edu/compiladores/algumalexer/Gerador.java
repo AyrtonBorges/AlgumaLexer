0c7ea9880d1150bf66b2b698b0a7c61ac0753289
EdivanAzevedo <66445757+EdivanAzevedo@users.noreply.github.com>
2023-07-06 21:48:00 -0300

    Update Gerador.java
    pajisoukhbgd,hbakiops´xç

--- src/main/java/br/iff/edu/compiladores/algumalexer/Gerador.java
+++ src/main/java/br/iff/edu/compiladores/algumalexer/Gerador.java
@@ -10,39 +10,40 @@ import br.iff.edu.compiladores.algumalexer.AlgumaParser.PageContext;
  *
  * @author ayrtonborges
  */
-public class Gerador extends AlgumaBaseVisitor<Void>
-{
-       StringBuilder saida;
-       
-       public Gerador()
-       {
-           this.saida = new StringBuilder();
-       }
+public class Gerador extends AlgumaBaseVisitor<Void> {
+
+    StringBuilder saida;
+
+    public Gerador() {
+        this.saida = new StringBuilder();
+    }
 
     @Override
     public Void visitPage(PageContext ctx) {
-           saida.append("<!DOCTYPE html>\n" +
-                        "<html lang=\"pt-br\">\n" +
-                        "    <head>\n" +
-                        "        <meta charset=\"UTF-8\"/>\n" + 
-                        "           <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
-                        "           <title>CSL - Site</title>\n" + 
-                        "    </head>\n" +
-                        "<body>");
-           visitHeader(ctx.header());
-           visitCorpo(ctx.corpo());
-           visitPernas(ctx.pernas());
-           saida.append("</body></html>");
-           
+        saida.append("<!DOCTYPE html>\n"
+                + "<html lang=\"pt-br\">\n"
+                + "    <head>\n"
+                + "        <meta charset=\"UTF-8\"/>\n"
+                + "           <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+                + "           <title>CSL - Site</title>\n"
+                + "    </head>\n"
+                + "<body>");
+        visitHeader(ctx.header());
+        visitCorpo(ctx.corpo());
+        visitPernas(ctx.pernas());
+        saida.append("</body></html>");
+
         return super.visitPage(ctx); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
     }
 
     @Override
     public Void visitHeader(AlgumaParser.HeaderContext ctx) {
         saida.append("<header>");
-        visitFuncoes(ctx.funcoes(0));
+        for (int i = 0; i < ctx.funcoes().size(); i++) {
+            visitFuncoes(ctx.funcoes(0));
+        }
         saida.append("</header>\n");
-        
+
         return super.visitHeader(ctx); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
     }
 
@@ -55,7 +56,9 @@ public class Gerador extends AlgumaBaseVisitor<Void>
     @Override
     public Void visitCorpo(AlgumaParser.CorpoContext ctx) {
         saida.append("<main>");
-        visitFuncoes(ctx.funcoes(0));
+        for (int i = 0; i < ctx.funcoes().size(); i++) {
+            visitFuncoes(ctx.funcoes(0));
+        }
         saida.append("</main>\n");
         return super.visitCorpo(ctx); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
     }
@@ -63,15 +66,19 @@ public class Gerador extends AlgumaBaseVisitor<Void>
     @Override
     public Void visitPernas(AlgumaParser.PernasContext ctx) {
         saida.append("<footer>");
-        visitFuncoes(ctx.funcoes(0));
-        saida.append( "</footer>\n");
+        for (int i = 0; i < ctx.funcoes().size(); i++) {
+            visitFuncoes(ctx.funcoes(0));
+        }
+        saida.append("</footer>\n");
         return super.visitPernas(ctx); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
     }
 
     @Override
     public Void visitTexto(AlgumaParser.TextoContext ctx) {
         saida.append("<article>");
-        visitValor_texto(ctx.valor_texto(0));
+        for (int i = 0; i < ctx.valor_texto().size(); i++) {
+            visitValor_texto(ctx.valor_texto(0));
+        }
         saida.append("</article>\n");
         return super.visitTexto(ctx); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
     }
@@ -90,9 +97,9 @@ public class Gerador extends AlgumaBaseVisitor<Void>
 
     @Override
     public Void visitSelo(AlgumaParser.SeloContext ctx) {
-        String entrada = "<img src=\""+ctx.url().getText()+"\" width=\"75\" height=\"75\" style=\"float:";
+        String entrada = "<img src=" + ctx.url().getText() + " width=\"75\" height=\"75\" style=\"float:";
         String tempPos = ctx.POSICAO().getText();
-        switch(tempPos){
+        switch (tempPos) {
             case "1":
                 entrada += "right;\">\n";
                 break;
@@ -108,28 +115,27 @@ public class Gerador extends AlgumaBaseVisitor<Void>
 
     @Override
     public Void visitImage(AlgumaParser.ImageContext ctx) {
-        String entrada = "<img src=\""+ctx.url().getText();
+        String entrada = "<img src=\"" + ctx.url().getText();
         String tam = ctx.TAMANHO().getText();
-        switch(tam){
+        switch (tam) {
             case "p":
                 entrada += "height=\"100\" width=\"100\" style=\"display: block; margin: auto;\">\n";
                 break;
-            
+
             case "m":
-                entrada += "height=\"250\" width=\"250\" style=\"display: block; margin: auto;\">\n"; 
+                entrada += "height=\"250\" width=\"250\" style=\"display: block; margin: auto;\">\n";
                 break;
-            
+
             case "g":
                 entrada += "height=\"400\" width=\"400\" style=\"display: block; margin: auto;\">\n";
                 break;
-            
+
             default:
                 entrada += ">";
         }
-        
+
         saida.append(entrada);
         return super.visitImage(ctx); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
     }
-      
-}
 
+}
